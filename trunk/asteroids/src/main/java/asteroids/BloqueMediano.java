8a490518f97ef321ce722b4bd90245501779eadd
loiaconopablo@gmail.com <loiaconopablo@gmail.com@069ade88-da3e-0667-798e-6201a866bc7e>
2014-05-12 22:45:06 +0000

    Cambios nuevos en clase

---
+++ trunk/asteroids/src/main/java/asteroids/BloqueMediano.java
@@ -0,0 +1,14 @@
+package asteroids;
+
+import java.awt.Color;
+
+import com.uqbar.vainilla.appearances.Rectangle;
+
+public class BloqueMediano extends Bloque{
+
+	public BloqueMediano(int x, int y) {
+		super(new Rectangle(Color.GREEN, 50,50), x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+}
