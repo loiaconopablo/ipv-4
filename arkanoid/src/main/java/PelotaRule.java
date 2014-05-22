bd26f133b0e0c5e386a5030ac2f6368ae3b094b9
marianogvarela@gmail.com <marianogvarela@gmail.com@069ade88-da3e-0667-798e-6201a866bc7e>
2014-05-22 14:17:26 +0000

    Tag de la entrega del asteroids

--- arkanoid/src/main/java/PelotaRule.java
+++
@@ -1,8 +0,0 @@
-
-
-
-public interface PelotaRule {
-
-	boolean mustApply(Pelota pelota, Vector2D nuevaPosicion, ArkanoidScene scene);
-	void apply(Pelota pelota, Vector2D nuevaPosicion, ArkanoidScene scene);
-}
